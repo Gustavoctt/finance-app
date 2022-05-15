529c38147a904c1872c4ead7f304a94b52d089f8
Gustavoctt <gustavocarrertartare@gmail.com>
2022-05-15 19:38:48 -0300

    Created Dashboard components

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,35 @@
+import { Continer } from "./styles";
+
+export function TrasactionsTable(){
+  return(
+    <Continer>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td> Desenvolvimento de site </td>
+            <td className="deposit"> R$ 1.000,00 </td>
+            <td> Desenvolvimento </td>
+            <td> 20/02/2021 </td>
+          </tr>
+
+          <tr>
+            <td> Aluguel </td>
+            <td className="withdraw"> - R$ 500,00 </td>
+            <td> Casa </td>
+            <td> 20/02/2021 </td>
+          </tr>
+
+        </tbody>
+      </table>
+    </Continer>
+  )
+}
