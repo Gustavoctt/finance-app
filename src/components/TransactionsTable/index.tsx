1f358631ded4735f6b236b4e5ae093d4153277ed
Gustavoctt <gustavocarrertartare@gmail.com>
2022-05-16 19:27:33 -0300

    created axios integration and modal component

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Continer } from "./styles";
 
 export function TrasactionsTable(){
+  useEffect(() => {
+    api.get('transactions')
+    .then(response => console.log(response.data))
+  }, [])
+
   return(
     <Continer>
       <table>
