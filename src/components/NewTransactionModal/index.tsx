c9a68d1e25e2b2e85a015e109a7b24ab4d711197
Gustavoctt <gustavocarrertartare@gmail.com>
2022-05-17 20:52:15 -0300

    created connection with api

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import Modal from "react-modal";
 
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
+import { api } from "../../services/api";
 
 import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 
@@ -15,7 +16,23 @@ interface NewTransactionModalProps{
 Modal.setAppElement('#root');
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps){
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
   const [ type, setType ] = useState('deposit');
+
+  function handleCreateNewTransaction(event: FormEvent){
+    event.preventDefault()
+
+    const data = {
+      title,
+      value,
+      type,
+      category,
+    };
+
+    api.post('/transactions', data)
+  }
 
   return(
     <Modal
@@ -33,16 +50,20 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         <img src={closeImg} alt="Fechar Modal" />
       </button>
 
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar informação</h2>
 
         <input 
           placeholder="Titulo"
+          value={title}
+          onChange={ event => setTitle(event.target.value) }
         />
 
         <input 
           type="number"
           placeholder="Valor"
+          value={value}
+          onChange={ event => setValue(Number(event.target.value)) }
         />
 
         <TransactionTypeContainer>
@@ -70,6 +91,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 
         <input 
           placeholder="Categoria"
+          value={category}
+          onChange={ event => setCategory(event.target.value) }
         />
 
         <button type="submit">
--- src/index.tsx
+++ src/index.tsx
@@ -2,23 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freela de site',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 2000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 800,
+          createdAt: new Date('2021-02-08 15:00:00')
+        }
+      ]
+    })
+  },
+
   routes(){
     this.namespace = 'api';
 
-    this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'transaction 1',
-          amout: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
+    this.get('transactions', (schema) => {
+      return schema.db.transactions
+    })
+
+    this.post('/transactions', (schema, request) =>{
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data);
     })
   }
 })
